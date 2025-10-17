51aba79eb57c29166104d7ab9f6459eb63b30248
Wagalidoom <lukas-8@live.fr>
2025-10-17 21:11:40 +0200

    feat: add farcaster

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { FHECounterDemo } from "./_components/FHECounterDemo";
+import { sdk } from '@farcaster/miniapp-sdk';
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      await sdk.actions.ready()
+    })()
+  }, [])
+  
   return (
     <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
       <FHECounterDemo />
